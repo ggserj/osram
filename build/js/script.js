32c21c985330dbb8c3b1519304b952a336a78931
ggserj <40269714+ggserj@users.noreply.github.com>
2018-08-06 04:44:34 +0300

    home block -mob

---
+++ build/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+	$('.advantages-slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		cssEase: 'ease-out',
+		speed: 300
+	});
+});
