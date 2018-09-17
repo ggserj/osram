ba34c7e0c88dce737945e6b0c199c9f87b13df09
ggserj <40269714+ggserj@users.noreply.github.com>
2018-09-17 08:34:31 +0300

    node

--- gulpfile.js
+++ gulpfile.js
@@ -2,33 +2,47 @@ const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const browserSync = require('browser-sync').create();
 const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
 
+const paths = {
+	html: {
+		src: 'src/html/**/*.html',
+		dest: 'build/'
+	},
+	css: {
+		src: 'src/css/**/*.styl',
+		dest: 'build/css/'
+	},
+	js: {
+		src: 'src/js/**/*.js',
+		dest: 'build/js/'
+	}
+};
 
-gulp.task('stylus', function() {
-	return gulp.src('src/css/*.styl')
-		.pipe(sourcemaps.init())
-		.pipe(stylus())
-		.pipe(sourcemaps.write())
-		.pipe(gulp.dest('build/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}))
-});
-
-gulp.task('browser-sync', function() {
+function reload() {
 	browserSync.init({
 		server: {
 			baseDir: 'build'
 		}
 	})
-});
+}
 
+function css() {
+	return gulp.src(paths.css.src)
+		.pipe(sourcemaps.init())
+		.pipe(stylus())
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest(paths.css.dest))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+}
 
-gulp.task('watch', ['browser-sync', 'stylus'], function(){
-	gulp.watch('src/css/*.styl', ['stylus']);
-	gulp.watch('build/index.html', browserSync.reload);
-	gulp.watch('build/js/*.js', browserSync.reload);
-});
+function watch() {
+	gulp.watch(paths.html.src, browserSync.reload);
+	gulp.watch(paths.css.src, css);
+	gulp.watch(paths.js.src, browserSync.reload);
+}
 
 
 
